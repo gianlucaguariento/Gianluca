c62677b4fbe5bf5509b7188c69b8c0ead9fa150a
Gianluca Guariento <E-gianluca.guariento@msc.com>
2023-01-31 12:23:04 +0100

    commit branch develop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,MSC ACADEMY ,  ciao a tutti, bla bla bla");
+Console.WriteLine("week 5");
+Console.WriteLine("week 6");
